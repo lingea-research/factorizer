1aac67db87a8a4b18f7e8fc7ee4582c6a2ae0c67
Maksim Tikhonov <81774545+Saipade@users.noreply.github.com>
2023-07-25 10:07:25 +0200

    remake demo

--- demo.py
+++ demo.py
@@ -1,14 +1,22 @@
-from pyonmttok_wrapper import PyonmttokWrapper
+from pyonmttok_wrapper_over import PyonmttokWrapper
 
 
 def demo():
 	tokenizer = PyonmttokWrapper(model='/home/large/data/models/marian/csen_factored_pyonmttok_fs.20221118/pyonmttok/csen-lowered.spm',
 															 add_in=False, case_feature=True)
-	print(['Last Friday I😊 saw a spotted striped blue worm shake hands with a legless lizard.', '"That building is tall."'])
-	tokenized = tokenizer.tokenize(src=['Last Friday I😊 saw a spotted striped blue worm shake hands with a legless lizard.', '"That building is tall."'],
-																 constraints=[{(0,4): 'posledni', (5,11): 'Patek', (12,13): 'Ja', (13,14): '嘚', (37,41): 'modry'}, {(6,14): 'stavba', (0,1): '«', (23,24): '»'}],
-																 wskip=0.2, sskip=0.5)
-	print(tokenized)
+	input = ['Last Friday I😊 saw a spotted striped blue worm shake hands with a legless lizard.', '"That building is tall."']
+	constr = [{(0,4): 'posledni', (5,11): 'Patek', (12,13): 'Ja', (13,14): '嘚', (37,41): 'modry'}, {(6,14): 'stavba', (0,1): '«', (23,24): '»'}]
+	print('Input:', input)
+	print('Constraints:', constr)
+	wskip = 0.2
+	sskip = 0.5
+	tokenized = tokenizer.tokenize(input, constr, wskip, sskip)
+	print('Tokenized with constraints:', tokenized)
+	tokenized = tokenizer.tokenize(input)
+	print('Tokenized without constraints:', tokenized)
+	detokenized = tokenizer.detokenize(tokenized)
+	print('Detokenized:', detokenized)
+	print('Is detokenized equal to the input?', input==detokenized)
 
 
 if __name__ == '__main__':
